135623e950a81c1ddc5a0685d158a36f292339c2
Baliram Prajapati <baliramprajapativns2016@gmail.com>
2021-06-16 13:25:51 +0530

    add

--- djangoajaxcrud/settings.py
+++ djangoajaxcrud/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'i_%==r4mn2&nl!u*4p+4(=h=r$s&@jh_cmn6f)5%yel&bj&t_z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
